960f2b8951b19af794cb2632c27ef7fb78273078
Nick007 <mac@hakunamatata.local>
2016-06-24 17:01:49 +0800

    project
    create Project

---
+++ HTTPDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPDemo
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 lyq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
